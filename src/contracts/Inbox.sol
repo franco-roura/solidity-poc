fe9bd93d679974a1f0992d61c0b3cdb4275f9bb4
Franco Roura <franco@capicua.com.uy>
2021-09-04 01:04:10 -0300

    Add Lottery contract

--- src/contracts/Inbox.sol
+++ src/contracts/Inbox.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
+    address public myAddr;
 
     function Inbox(string initialMessage) public {
-        message = initialMessage;
+        message = initialMessage;    
     }
 
     function setMessage(string newMessage) public {
